78c7a61d7bce919276ca9cc7d91a3d373e52040f
sednaProje <veyselnalti32@gmail.com>
2023-05-27 17:48:59 +0300

    CarGames
    2114216024 CarGames

---
+++ Assets/ACC_Lite/Scripts/Game/GamePlay/UserControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (CarController))]
+public class UserControl :MonoBehaviour
+{
+
+	CarController ControlledCar;
+
+	public float Horizontal { get; private set; }
+	public float Vertical { get; private set; }
+	public bool Brake { get; private set; }
+
+	public static MobileControlUI CurrentUIControl { get; set; }
+
+	private void Awake ()
+	{
+		ControlledCar = GetComponent<CarController> ();
+		CurrentUIControl = FindObjectOfType<MobileControlUI> ();
+	}
+
+	void Update ()
+	{
+		if (CurrentUIControl != null && CurrentUIControl.ControlInUse)
+		{
+			
+			Horizontal = CurrentUIControl.GetHorizontalAxis;
+			Vertical = CurrentUIControl.GetVerticalAxis;
+		}
+		else
+		{
+			
+			Horizontal = SimpleInput.GetAxis ("Horizontal");
+			Vertical = SimpleInput.GetAxis ("Vertical");
+			Brake = SimpleInput.GetButton ("Jump");
+		}
+
+		
+		ControlledCar.UpdateControls (Horizontal, Vertical, Brake);
+	}
+}
